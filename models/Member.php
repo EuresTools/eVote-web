1e4c9e77d85e5a1d96715d4d4cb1d25b0211697b
Bjorn Orri <bjornorri@gmail.com>
2015-07-02 14:52:32 +0200

    Create member model

---
+++ models/Member.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "member".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $group
+ * @property integer $poll_id
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Code[] $codes
+ * @property Contact[] $contacts
+ * @property Poll $poll
+ */
+class Member extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'member';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'poll_id'], 'required'],
+            [['poll_id'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['name', 'group'], 'string', 'max' => 255]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'group' => 'Group',
+            'poll_id' => 'Poll ID',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    public function beforeSave($insert) {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->created_at = new \yii\db\Expression('NOW()');
+            }
+            $this->updated_at = new \yii\db\Expression('NOW()');
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCodes()
+    {
+        return $this->hasMany(Code::className(), ['member_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getContacts()
+    {
+        return $this->hasMany(Contact::className(), ['member_id' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPoll()
+    {
+        return $this->hasOne(Poll::className(), ['id' => 'poll_id']);
+    }
+}
