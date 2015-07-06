4fc517540f0f72545e0e80b8997d4766df2a0f4a
Bjorn Orri <bjornorri@gmail.com>
2015-07-06 13:44:15 +0200

    ...

--- migrations/m150706_113400_add_admin_account.php
+++ migrations/m150706_113400_add_admin_account.php
@@ -10,6 +10,7 @@ class m150706_113400_add_admin_account extends Migration
         $this->insert('user', ['username' => 'admin',
             'password_hash' => '$2y$13$tGXqc0xkHdB89no1kdFnqujEsDufvJKRLg62CtznRCcWeMRJIz81a',
             'is_admin' => true,
+            'auth_key' => '1GUCDO17QosEIa-NZdmShGKce5gKX4Xo',
             'created_at' => new \yii\db\Expression('NOW()'),
             'updated_at' => new \yii\db\Expression('NOW()'),
         ]);
