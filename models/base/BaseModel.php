0028d6bf36c1275d1ae1b6850850febb39ee96cc
Bjorn Orri <bjornorri@gmail.com>
2015-07-06 16:47:19 +0200

    Fix errors in base model

--- models/base/BaseModel.php
+++ models/base/BaseModel.php
@@ -4,9 +4,11 @@ namespace app\models\base;
 
 use Yii;
 use yii\behaviors\TimestampBehavior;
+use yii\behaviors\AttributeBehavior;
 use yii\db\Expression;
+use yii\db\ActiveRecord;
 
-class BaseModel extends \yii\db\ActiveRecord
+class BaseModel extends ActiveRecord
 {
 
     public function behaviors()
@@ -15,8 +17,8 @@ class BaseModel extends \yii\db\ActiveRecord
             [
                 'class' => AttributeBehavior::className(),
                 'attributes' => [
-                    ActiveRecord::EVENT_BEFORE_INSERT => 'created_by',
-                    ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_by',
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_by'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_by'],
                 ],
                 'value' => function ($event) {
                     return Yii::$app->user->id;
@@ -24,9 +26,14 @@ class BaseModel extends \yii\db\ActiveRecord
             ],
             [
                 'class' => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+
                 // 'createdAtAttribute' => 'create_time',
                 // 'updatedAtAttribute' => 'update_time',
-                'value' => new Expression('NOW()'),
+                //'value' => new Expression('NOW()'),
             ],
         ];
     }
