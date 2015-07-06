dfa75f076c63f491ff7fb8d9d4bd108fd7befef6
Benjamin Hoft <hoft@eurescom.eu>
2015-07-06 15:35:35 +0200

    add automatically updated_by and created_by by behaviour

--- models/base/BaseModel.php
+++ models/base/BaseModel.php
@@ -13,6 +13,16 @@ class BaseModel extends \yii\db\ActiveRecord
     {
         return [
             [
+                'class' => AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'created_by',
+                    ActiveRecord::EVENT_BEFORE_UPDATE => 'updated_by',
+                ],
+                'value' => function ($event) {
+                    return Yii::$app->user->id;
+                },
+            ],
+            [
                 'class' => TimestampBehavior::className(),
                 // 'createdAtAttribute' => 'create_time',
                 // 'updatedAtAttribute' => 'update_time',
