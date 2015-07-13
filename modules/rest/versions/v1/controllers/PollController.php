0b21487fc7f7ae0853facc50f46ac6edab8f3e64
Benjamin Hoft <hoft@eurescom.eu>
2015-07-13 16:17:21 +0200

    changed the v1 controller behavior

--- modules/rest/versions/v1/controllers/PollController.php
+++ modules/rest/versions/v1/controllers/PollController.php
@@ -3,40 +3,12 @@
 namespace app\modules\rest\versions\v1\controllers;
 
 use Yii;
-use yii\rest\ActiveController;
 use app\models\Poll;
-use yii\web\Response;
-use yii\helpers\ArrayHelper;
-use yii\filters\ContentNegotiator;
+use app\components\controllers\VotingRestController;
 
-class PollController extends ActiveController
+class PollController extends VotingRestController
 {
     public $modelClass = 'app\models\Poll';
 
-    /*
-    public function behaviors()
-    {
-        //set response header to application/json only
-        return [
-            'contentNegotiator' => [
-                'class' => ContentNegotiator::className(),
-                'formats' => [
-                    'application/json' => Response::FORMAT_JSON,
-        //            'application/xml' => Response::FORMAT_XML,
-                ],
-            ],
-        ];
-    }*/
 
-
-    /**
-    * disable session for REST-Request
-    * no loginUrl required
-    */
-    public function init2()
-    {
-        parent::init();
-        \Yii::$app->user->enableSession = false;
-        \Yii::$app->user->loginUrl = null;
-    }
 }
