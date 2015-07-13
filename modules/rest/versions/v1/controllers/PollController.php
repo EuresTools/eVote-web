3b1cb74b86517d82573eb66f3ece48e8358ddd43
Benjamin Hoft <hoft@eurescom.eu>
2015-07-13 13:12:38 +0200

    added behavior to support only json reponse (commented out as we leave it as it is for the time beeing so both json and xml response is supported)

--- modules/rest/versions/v1/controllers/PollController.php
+++ modules/rest/versions/v1/controllers/PollController.php
@@ -6,10 +6,28 @@ use Yii;
 use yii\rest\ActiveController;
 use app\models\Poll;
 use yii\web\Response;
+use yii\helpers\ArrayHelper;
+use yii\filters\ContentNegotiator;
 
 class PollController extends ActiveController
 {
     public $modelClass = 'app\models\Poll';
+
+    /*
+    public function behaviors()
+    {
+        //set response header to application/json only
+        return [
+            'contentNegotiator' => [
+                'class' => ContentNegotiator::className(),
+                'formats' => [
+                    'application/json' => Response::FORMAT_JSON,
+        //            'application/xml' => Response::FORMAT_XML,
+                ],
+            ],
+        ];
+    }*/
+
 
     /**
     * disable session for REST-Request
