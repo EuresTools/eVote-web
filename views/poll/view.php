57dc271a78d73897cb57d4d5d0f50b2dcac29a17
Bjorn Orri <bjornorri@gmail.com>
2015-07-27 15:45:58 +0200

    Create tabs on poll page

--- views/poll/view.php
+++ views/poll/view.php
@@ -8,6 +8,7 @@ use app\models\MemberSearch;
 use app\components\helpers\PollUrl;
 use app\models\Code;
 use app\models\Poll;
+use kartik\tabs\TabsX;
 
 /**
  * @var yii\web\View $this
@@ -18,76 +19,40 @@ $this->title = $model->__toString();
 $this->params['breadcrumbs'][] = ['label' => Poll::label(2), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+
 <div class="poll-view">
     <div class="page-header">
         <h1><?= Html::encode($this->title) ?></h1>
     </div>
-    <p>
-        <?= Html::a(Yii::t('yii', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('yii', 'Delete'), ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
-
-
     <?php
 
-    // Populate the attribute array for display.
-    $attributes = ['question:ntext'];
-    foreach ($model->getOptions()->all() as $index => $option) {
-            $no = $index + 1;
-            $attributes[] = ['attribute' => "Option $no", 'value' => $option->text];
-    }
-
-    ?>
-
-    <?= Html::tag('h2', Poll::label(1)); ?>
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => $attributes,
-    ]) ?>
-
-
-    <?= Html::tag('h2', Yii::t('app', 'About')) ?>
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            //'id',
-            'title',
-            //'question:ntext',
-            [
-                'attribute' => 'organizer',
-                'format' => 'raw',
-                'value' => Yii::$app->user->identity->isAdmin() ? Html::a(Html::encode($model->getOrganizer()->one()->name), ['organizer/view', 'id' => $model->getOrganizer()->one()->id]) : Html::encode($model->getOrganizer()->one()->name),
-            ],
-            'select_min',
-            'select_max',
-            'start_time:datetime',
-            'end_time:datetime',
-            'created_at:datetime',
-            'updated_at:datetime',
-            // 'created_by',
-            // 'updated_by',
-            'creator',
-            'editor',
+    $items = [
+        [
+            'label' => '<i class="glyphicon glyphicon-info-sign"></i> About',
+            'content' => $this->render('_about_tab', ['model' => $model]),
+            'active' => true,
         ],
-    ])?>
-    <?php
+        [
+            'label' => '<i class="glyphicon glyphicon-user"></i> Members',
+            'content' => $this->render('_members_tab', [
+                'model' => $model,
+                'memberDataProvider' => $memberDataProvider,
+                'memberSearchModel' => $memberSearchModel,
+            ]),
+        ],
+        [
+            'label' => '<i class="glyphicon glyphicon-signal"></i> Results',
+            'content' => $this->render('_options_tab', ['model' => $model]),
+        ],
+    ];
 
-    ?>
-
-    <?php
-
-    echo $this->render('_members_view', [
-        'model'=>$model,
-        'memberDataProvider'=>$memberDataProvider,
-        'memberSearchModel'=>$memberSearchModel
+    echo TabsX::widget([
+        'items' => $items,
+        'position' => TabsX::POS_ABOVE,
+        'encodeLabels' => false
     ]);
 
-    echo $this->render('_options_view', ['model'=>$model]);
+
+    //echo $this->render('_options_view', ['model'=>$model]);
     ?>
 </div>
