9f5482a1280d04ee8047998ea7b99adce5af2563
Benjamin Hoft <hoft@eurescom.eu>
2015-07-31 15:25:57 +0200

    PSR2 coding style fixes

--- models/forms/UploadForm.php
+++ models/forms/UploadForm.php
@@ -5,18 +5,21 @@ namespace app\models\forms;
 use yii\base\Model;
 use yii\web\UploadedFile;
 
-class UploadForm extends Model {
+class UploadForm extends Model
+{
 
     public $excelFile;
 
-    public function rules() {
+    public function rules()
+    {
         return [
             [['excelFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'xls, xlsx', 'checkExtensionByMimeType' => true],
         ];
     }
 
-    public function upload() {
-        if($this->validate()) {
+    public function upload()
+    {
+        if ($this->validate()) {
             return $this->excelFile;
         }
         return false;
