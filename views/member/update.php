efdde6c3e69a409e0395faf5e770ea003d43fb66
Bjorn Orri <bjornorri@gmail.com>
2015-08-05 10:36:38 +0200

    Fix more breadcrumbs

--- views/member/update.php
+++ views/member/update.php
@@ -13,7 +13,7 @@ $this->title = Yii::t('app', 'Update {modelClass}: ', [
 ]) . ' ' . $model->__toString();
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Polls'), 'url' => ['poll/index']];
 $this->params['breadcrumbs'][] = ['label' => $model->poll, 'url' => ['poll/view', 'id' => $model->poll_id]];
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Members'), 'url' => PollUrl::toRoute(['member/index', 'poll_id' => $model->poll_id])];
+//$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Members'), 'url' => PollUrl::toRoute(['member/index', 'poll_id' => $model->poll_id])];
 $this->params['breadcrumbs'][] = ['label' => $model->__toString(), 'url' => ['member/view', 'id' => $model->id, 'poll_id' => $model->poll_id]];
 $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 
