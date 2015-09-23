f4933c359db7e7404db806326aa11133ef4721e3
Benjamin Hoft <hoft@eurescom.eu>
2015-09-23 13:48:27 +0200

    layout change on contact form

--- views/site/contact.php
+++ views/site/contact.php
@@ -33,7 +33,7 @@ $this->params['breadcrumbs'][] = $this->title;
                 <?= $form->field($model, 'subject') ?>
                 <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
                 <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
+                    'template' => '<div class="row"><div class="col-lg-offset-2  col-lg-4">{image}</div><div class="col-lg-6">{input}</div></div>',
                 ]) ?>
                 <div class="form-group">
                     <?= Html::submitButton(Yii::t('app', 'Submit'), ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
