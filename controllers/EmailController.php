e214fae96287c3fa679cf70c2c1a02cb264f6f9e
Benjamin Hoft <hoft@eurescom.eu>
2015-09-25 09:53:00 +0200

    Tokens in Emails sent by non Admins where scambled because toString method was used.

--- controllers/EmailController.php
+++ controllers/EmailController.php
@@ -112,7 +112,7 @@ class EmailController extends PollDependedController
     {
         $string = str_replace('<member-name>', $member->name, $string);
         $string = str_replace('<member-group>', $member->group, $string);
-        $string = str_replace('<voting-code>', $member->getValidCode(), $string);
+        $string = str_replace('<voting-code>', $member->getValidCode()->getAttribute('token'), $string);
         return $string;
     }
 }
