83d93d44eeedad1d9d3b04188cf40c2f2d978473
Benjamin Hoft <hoft@eurescom.eu>
2015-10-01 15:13:03 +0200

    fixed the ExcelParser issues (multiple contacts were not imported, columns could not be reordered)

--- components/ExcelParser.php
+++ components/ExcelParser.php
@@ -7,9 +7,11 @@ use \PHPExcel;
 use \PHPExcel_IOFactory;
 use yii\helpers\ArrayHelper;
 
-class ExcelParser {
+class ExcelParser
+{
 
-    public static function parseMembers($filepath) {
+    public static function parseMembers($filepath)
+    {
         $file = file_get_contents($filepath);
         $workbook = PHPExcel_IOFactory::load($filepath);
         $worksheet = self::findSheet($workbook);
@@ -21,37 +23,44 @@ class ExcelParser {
         $member = null;
         foreach ($worksheet->getRowIterator() as $row) {
             $currRow++;
-            if(!$topFound) {
-                if(self::isTopRow($row)) {
+            if (!$topFound) {
+                if (self::isTopRow($row)) {
                     $columns = self::getColumns($worksheet, $row);
                     $topFound = true;
                 }
             } else {
-                $name = self::getCellValue($worksheet, $currRow, ArrayHelper::getValue($columns, 'name'));
-                if (trim(strtolower($name)) !== trim(strtolower($prevName))) {
-                    if($member !== null) {
-                        $members[] = $member;
-                    }
-                    $member = [];
-                    $member['row'] = $currRow + 1;
-                    $member['name'] = $name;
-                    $member['group'] = self::getCellValue($worksheet, $currRow, ArrayHelper::getValue($columns, 'group'));
-                    $member['contacts'] = [];
+                $name = trim(self::getCellValue($worksheet, $currRow, ArrayHelper::getValue($columns, 'name')));
+                $index_name = strtolower($name);
+                if ($index_name) {
 
+                    if (!isset($members[$index_name])) {
+                        // add member to members list
+                        $member = [];
+                        $member['row'] = $currRow + 1;
+                        $member['rows'][] = $currRow + 1;
+                        $member['name'] = trim($name);
+                        $member['group'] = self::getCellValue($worksheet, $currRow, ArrayHelper::getValue($columns, 'group'));
+                        $member['contacts'] = [];
+                        $members[$index_name] = $member; // add member to members list
+                    } else {
+                        $members[$index_name]['rows'][] = $currRow + 1;
+                    }
+
+                    // go through the contacts of this line and add it to the member entry by the index name
                     $tups =[[$columns['contact1'], $columns['email1']], [$columns['contact2'], $columns['email2']]];
                     foreach ($tups as $tup) {
                         list($contact_col, $email_col) = $tup;
                         $emailValue = trim(self::getCellValue($worksheet, $currRow, $email_col));
-                        if($emailValue !== null && $emailValue !== '') {
+                        if ($emailValue !== null && $emailValue !== '') {
                             $contact = [];
                             $contact['row'] = $currRow + 1;
                             $contact['email'] = $emailValue;
 
-                            $nameValue = self::getCellValue($worksheet, $currRow, $contact_col);
-                            if($nameValue !== null && $nameValue !== '') {
+                            $nameValue = trim(self::getCellValue($worksheet, $currRow, $contact_col));
+                            if ($nameValue !== null && $nameValue !== '') {
                                 $contact['name'] = $nameValue;
                             }
-                            $member['contacts'][] = $contact;
+                            $members[$index_name]['contacts'][] = $contact;
                         }
                     }
                 }
@@ -59,23 +68,29 @@ class ExcelParser {
             }
         }
         if (!$topFound) {
-            return False;
+            return false;
         }
+        //print_pre($columns, 'columns', false , false);
+        //print_pre($topFound, 'topFound', false , false);
+        //print_pre($members,'members',false, false, 100);
         return $members;
     }
 
-
-    /* Get the value of a cell indexed by row and column number starting from 0 
-        * */
-    private static function getCellValue($worksheet, $row, $column) {
+    /*
+    * Get the value of a cell indexed by row and column number starting from 0
+    */
+    private static function getCellValue($worksheet, $row, $column)
+    {
         $row++;
         return $worksheet->getCellByColumnAndRow($column, $row)->getCalculatedValue();
     }
 
 
-    /* Delete all word-separating characters, make lowercase and write organization with z, not s. 
-     * */
-    private static function cleanString($string) {
+    /*
+    * Delete all word-separating characters, make lowercase and write organization with z, not s.
+    */
+    private static function cleanString($string)
+    {
         $string = strtolower($string);
         $separators = [' ', '_', '-'];
         $string = str_replace($separators, '', $string);
@@ -84,13 +99,15 @@ class ExcelParser {
     }
 
     /* Find the relevant sheet and return it. */
-    private static function findSheet($workbook) {
+    private static function findSheet($workbook)
+    {
         $worksheet = $workbook->getActiveSheet();
         return $worksheet;
     }
 
     /* Returns the index of the header row. */
-    private static function findTopRow($worksheet) {
+    private static function findTopRow($worksheet)
+    {
         $expectedValues = ['organization', 'stakeholdergroup', 'contactperson1', 'contactperson2', 'emailaddress1', 'emailaddress2'];
         $currRow = -1;
         foreach ($worksheet->getRowIterator() as $row) {
@@ -98,7 +115,7 @@ class ExcelParser {
             $values = [];
             $currCell = -1;
 
-            foreach($row->getCellIterator() as $cell) {
+            foreach ($row->getCellIterator() as $cell) {
                 $value = self::cleanString($cell->getCalculatedValue());
                 $values[] = $value;
             }
@@ -117,7 +134,8 @@ class ExcelParser {
         return -1;
     }
 
-    private static function isTopRow($row) {
+    private static function isTopRow($row)
+    {
         $expectedValues = [
             'organization',
             'stakeholdergroup',
@@ -127,7 +145,7 @@ class ExcelParser {
             'emailaddress2'
         ];
         $values = [];
-        foreach($row->getCellIterator() as $cell) {
+        foreach ($row->getCellIterator() as $cell) {
             $value = self::cleanString($cell->getCalculatedValue());
             $values[] = $value;
         }
@@ -143,7 +161,8 @@ class ExcelParser {
     }
 
     /* Returns the indices of the relevant columns. */
-    private static function getColumns($worksheet, $topRow) {
+    private static function getColumns($worksheet, $topRow)
+    {
         $columns = [
             'name' => 'organization',
             'group' => 'stakeholdergroup',
@@ -157,9 +176,17 @@ class ExcelParser {
         foreach ($topRow->getCellIterator() as $cell) {
             $currCell++;
             $cellValue = $cell->getCalculatedValue();
-            if(is_string($cellValue)) {
+            if (is_string($cellValue)) {
+
                 $cellValue = self::cleanString($cellValue);
-                if($key = array_search($cellValue, $columns)) {
+                // print_pre($cellValue,'cellValue',false);
+                // print_pre($columns,'columns',false);
+
+                $key = array_search($cellValue, $columns, true);
+                // print_pre($key,'key',false);
+                // print_pre($currCell,'currCell',false);
+                // print_pre('-----','',false);
+                if ($key !==false) {
                     $columns[$key] = $currCell;
                 }
             }
